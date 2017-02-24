1be7a438b6aee9f36ed8fe54c1031ad220525636
Lucas BILLET <lucas.billet@free.fr>
2017-02-24 09:25:27 +0100

    Exercise 1

--- test/program.test.js
+++ test/program.test.js
@@ -2,11 +2,37 @@ var expect = require('chai').expect;
 
 var transformCheckpoint = require('../src/program').transformCheckpoint;
 
+var param = {
+  id: 'whataw0nd3rful1d',
+  uuid: 'whataw0nd3rful1d',
+  address: 'unknown',
+  addressType: 'unknown',
+  connectable: true,
+  advertisement: {
+    localName: undefined,
+    txPowerLevel: undefined,
+    manufacturerData: undefined,
+    serviceData: [],
+    serviceUuids: [ 'abcd' ]
+  },
+  rssi: -66,
+  services: null,
+  state: 'outofcontrol'
+}
+
 
 describe('Function transformCheckpoint', function() {
 
   it('Function transformCheckpoint without parameter should return false', function() {
     expect(transformCheckpoint()).to.be.false;
+  });
+
+});
+
+describe('Parameter mutated with transformCheckpoint', function() {
+
+  it('Parameter should be mutated with the function transformCheckpoint', function() {
+    expect(param).to.not.eql(transformCheckpoint(param));
   });
 
 });
